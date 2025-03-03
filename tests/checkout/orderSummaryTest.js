ca486613dcece05f36b0337a6a3b5e4d4580c5b3
BachNguyen264 <nguyenhienbach264@gmail.com>
2025-03-03 21:56:29 +0700

    TESTING EXERCISE

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { cart, loadFromStorage } from "../../data/cart.js";
+import { products } from "../../data/products.js";
 
 describe('test suite: renderOrderSummary', ()=>{
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
@@ -35,7 +36,12 @@ describe('test suite: renderOrderSummary', ()=>{
         expect(
             document.querySelector(`.js-product-quantity-${productId2}`).innerText
         ).toContain('Quantity: 1');
-        document.querySelector('.js-test-container').innerHTML = '';
+        expect(
+            document.querySelector(`.js-product-name-${productId1}`).innerText    
+        ).toEqual(products.find(product=> product.id === productId1).name); 
+        expect(
+            document.querySelector(`.js-product-name-${productId2}`).innerText    
+        ).toEqual(products.find(product=> product.id === productId2).name);    
     });
 
     it('removes a product',()=>{
@@ -51,6 +57,9 @@ describe('test suite: renderOrderSummary', ()=>{
         ).not.toEqual(null);
         expect(cart.length).toEqual(1);
         expect(cart[0].productId).toEqual(productId2);
+    });
+
+    afterEach(()=>{
         document.querySelector('.js-test-container').innerHTML = '';
     });
 });
--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,42 +1,115 @@
-import { addToCart, cart, loadFromStorage } from "../../data/cart.js";
+import {
+  addToCart,
+  cart,
+  loadFromStorage,
+  removeFromCart,
+} from "../../data/cart.js";
+
+const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
+const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
 
 describe("test suite: addToCart", () => {
-  beforeEach(()=>{
-    document.querySelector('.js-test-container').innerHTML = `
+  beforeEach(() => {
+    document.querySelector(".js-test-container").innerHTML = `
       <input type="number" value="1" class="js-quantity-selector-e43638ce-6aa0-4b85-b27f-e1d07eb678c6">
     `;
-  })
+    spyOn(localStorage, "setItem");
+  });
+
   it("adds an existing product to the cart", () => {
-    spyOn(localStorage,'setItem');
-    spyOn(localStorage, 'getItem').and.callFake(()=>{
-        return JSON.stringify([{
-            productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-            quantity: 1,
-            deliveryOptionId: '1'
-        }]);
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ]);
     });
     loadFromStorage();
 
-    addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    addToCart(productId1);
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].quantity).toEqual(2)
-    document.querySelector('.js-test-container').innerHTML = '';
+    expect(cart[0].productId).toEqual(productId1);
+    expect(cart[0].quantity).toEqual(2);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "cart",
+      JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+      ])
+    );
+    document.querySelector(".js-test-container").innerHTML = "";
   });
 
   it("adds a new product to the cart", () => {
-    spyOn(localStorage, 'setItem');
-
-    spyOn(localStorage, 'getItem').and.callFake(()=>{
-        return JSON.stringify([]);
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([]);
     });
     loadFromStorage();
-    addToCart("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    addToCart(productId1);
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].quantity).toEqual(1)
-    document.querySelector('.js-test-container').innerHTML = '';
+    expect(cart[0].productId).toEqual(productId1);
+    expect(cart[0].quantity).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "cart",
+      JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ])
+    );
+    document.querySelector(".js-test-container").innerHTML = "";
+  });
+});
+
+describe("test suite: removeFromCart", () => {
+  beforeEach(() => {
+    spyOn(localStorage, "setItem");
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: productId1,
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ]);
+    });
+    loadFromStorage();
+  });
+
+  it("remove a productId in the cart", () => {
+    removeFromCart(productId1);
+    expect(cart.length).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith(
+      "cart",
+      JSON.stringify([
+        {
+          productId: productId2,
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ])
+    );
+  });
+
+  it("remove a productId not in the cart", () => {
+    removeFromCart("abc-xyz-aloha-123");
+    expect(cart.length).toEqual(2);
+    expect(localStorage.getItem).toHaveBeenCalledTimes(2);  
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
   });
 });
