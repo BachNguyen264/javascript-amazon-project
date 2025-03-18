8017965ba58118a9eac63ffc02adb3a1d36df876
BachNguyen264 <nguyenhienbach264@gmail.com>
2025-03-18 22:32:13 +0700

    learn error handling

--- data/products.js
+++ data/products.js
@@ -115,9 +115,13 @@ export function loadProductsFetch(){
     });
 
     console.log('Load products');
+  }).catch((error)=>{
+    console.log('Unexpected error. Please try again later.');  
   });
+  
     return promise;
 }
+
 /*
 loadProductsFetch().then(()=>{
   console.log('next step');
@@ -141,9 +145,15 @@ export function loadProducts(fun){
 
     fun();
   });
+
+  xhr.addEventListener('error', (error)=> {
+    console.log('Unexpected error. Please try again later.');
+  });
+
   xhr.open('GET','https://supersimplebackend.dev/products');
   xhr.send();
 }
+
 
 /*
 export const products = [
